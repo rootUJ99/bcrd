e5727acff280c172c18ec9666473ec5bf5a283f4
UJwal Arak <rootUJ@localhost.localdomain>
2018-03-12 02:35:41 +0530

    all ready

--- src/App.js
+++ src/App.js
@@ -125,11 +125,13 @@ class App extends Component {
               </section>
               <div className="line"></div>
             </section>
-          <button className="scroll-down">
+          <div className="wrapper">
+            <button className="scroll-down">
 
             <NavLink to="/#scrollup" scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })}> <i className="fas fa-angle-down icon-size "></i> </NavLink>
 
           </button>
+          </div>
         </header>
         
       </div>
--- src/Note.js
+++ src/Note.js
@@ -5,24 +5,24 @@ const Note =()=>(
         <div className="notebook">
             <p>So it's your birthday all again and you haven't grown even a centimeter :p </p>
             <p>Well favdi i must say its been quite years now since we are friends i still remember days
-             you were used to ignore me :p. In this jouerny we became friends then maybe bestfriends and now
+             you were used to ignore me :p. In this journey we became friends then maybe bestfriends and now
              strangers? nah,<b> Once Bestfriends always Bestfriends</b>. you might be thinking that I'm the strangest
              person you have ever met, sometimes overly expressive,  sometimes not at all expressive, yeah you can call 
              me wierd </p>
-            <p>So for few sentenses im gonna put my heart out, Trust me i always want to talk to you, yet i dont. 
+            <p>So for today I'm gonna put my heart out, Trust me i always want to talk to you, yet i dont. 
             sometimes i want to hangout with you, yet you don't, that's fine you have more good people to be consider into the
-             category of bestfriends. Maybe we were never bestfriends and we were just pretending to be bestfriends maybe i dont hold 
+             category of Bestfriends. Maybe we were never Bestfriends and we were just pretending to be Bestfriends maybe i don't hold 
             that importance in your life. But now it dosn't ever bothers me </p>
         </div>
         <div className="notebook">
-            <p>Really i dont want, you to ever leave Mumbai. Maybe I'm wierd maybe i dont want to fall 
+            <p>Really i dont want, you to ever leave Mumbai. Maybe I'm wierd, maybe i dont want to fall 
             in the trap of Love again but i still care for you. I don't understand people beacuse in reality i dont understand myself
             </p>
             <p>I wish someday we will talk about this all but yet i don't want that day to come. i'm totally wierd 
             i know that thanks to bare with me all the way along. Evern though i acted like a shitty crap with you, you 
-            handled it in calm maybe we are best friends</p>
-            <p>Anyway Sorry for bringing all the mess in your wishes, enjoy your 22nd birthday have a great life ahead sweetheart</p>
-            <p>P.S. I'm quite lazy so i have made this page quite cheaply and even i'm feeling quite lazy right now</p>
+            handled it in calm way maybe we are Bestfriends and i'm assuming all the crap. </p>
+            <p>Anyway Sorry for bringing all the mess in your birthday card, I'm glad to have you, enjoy your 22nd birthday have a great life ahead sweetheart</p>
+            <p>P.S. I'm quite lazy so i have made this page quite cheaply and even i'm feeling quite sleepy right now</p>
         </div>
     </div>
 );
